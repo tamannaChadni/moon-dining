1f1d0a14647fc091b6a39e7cda5c825ce7862c88
Tamanna Chadni <tamanna.chadni.eng@gmail.com>
2024-05-11 22:58:14 +0600

    singlePage of food details setup

--- src/component/FoodDetails/FoodDetails.jsx
+++ src/component/FoodDetails/FoodDetails.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const FoodDetails = () => {
+  const food = useLoaderData();
+  const {food_name,food_image,food_category,price,made_by,food_origin,description} = food;
     return (
-        <div>
-          <h2>hi</h2>  
+      <div className="container mx-auto mt-4">
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            alt=""
+            className="object-cover w-full h-52 dark:bg-gray-500"
+            src={food_image}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title font-bold text-2xl">{food_name}</h2>
+          <p className=" font-extralight"> {food_category}</p>
+          
+          <p className=" font-light">{ price}</p>
+          
+          <p className="font-semibold"> {made_by}</p>
+          
+          
+          <p className="font-semibold">
+            <span className="text-red-500">{food_origin}</span>
+          </p>
+          <p className="font-semibold">
+            <span className="text-red-500">{description}</span>
+          </p>
+          <button className='bg-red-700 text-white btn-sm btn'>Pruchase</button>
         </div>
+      </div>
+
+    </div>
     );
 };
 
